de2c5e672f2fe23ff425f7b1961c770769136b2c
DIVJOTPARMAR <118370178+DIVJOTPARMAR@users.noreply.github.com>
2024-06-09 10:00:07 +0530

    Update data_preprocessing.py

--- PRO-C119-Project-Boilerplate-main/data_preprocessing.py
+++ PRO-C119-Project-Boilerplate-main/data_preprocessing.py
@@ -14,15 +14,15 @@ import json
 import pickle
 import numpy as np
 
-words = []  # list of unique root words in the data
-classes = []  # list of unique tags in the data
-pattern_word_tags_list = []  # list of the pair of (['words', 'of', 'the', 'sentence'], 'tags')
+words=[] #list of unique roots words in the data
+classes = [] #list of unique tags in the data
+pattern_word_tags_list = [] #list of the pair of (['words', 'of', 'the', 'sentence'], 'tags')
 
 # words to be ignored while creating Dataset
-ignore_words = ['?', '!', ',', '.', "'s", "'m"]
+ignore_words = ['?', '!',',','.', "'s", "'m"]
 
 # open the JSON file, load data from it.
-train_data_file = open('PRO-C119-Project-Boilerplate-main/intents.json')
+train_data_file = open('PRO-C120-Project-Boilerplate-main/intents.json')
 data = json.load(train_data_file)
 train_data_file.close()
 
@@ -30,55 +30,80 @@ train_data_file.close()
 def get_stem_words(words, ignore_words):
     stem_words = []
     for word in words:
-        # Check if word is not a part of stop word
         if word not in ignore_words:
-            # lowercase it, stem it, and append it to stem_words list
-            stem_word = stemmer.stem(word.lower())
-            stem_words.append(stem_word)
+            w = stemmer.stem(word.lower())
+            stem_words.append(w)      
+
     return stem_words
+
+
+'''
+List of sorted stem words for our dataset : 
+
+['all', 'ani', 'anyon', 'are', 'awesom', 'be', 'best', 'bluetooth', 'bye', 'camera', 'can', 'chat', 
+'cool', 'could', 'digit', 'do', 'for', 'game', 'goodby', 'have', 'headphon', 'hello', 'help', 'hey', 
+'hi', 'hola', 'how', 'is', 'later', 'latest', 'me', 'most', 'next', 'nice', 'phone', 'pleas', 'popular', 
+'product', 'provid', 'see', 'sell', 'show', 'smartphon', 'tell', 'thank', 'that', 'the', 'there', 
+'till', 'time', 'to', 'trend', 'video', 'what', 'which', 'you', 'your']
+
+'''
+
 
 # creating a function to make corpus
 def create_bot_corpus(words, classes, pattern_word_tags_list, ignore_words):
+
     for intent in data['intents']:
+
         # Add all patterns and tags to a list
-        for pattern in intent['patterns']:
-            # tokenize the pattern
+        for pattern in intent['patterns']:  
+
+            # tokenize the pattern          
             pattern_words = nltk.word_tokenize(pattern)
+
             # add the tokenized words to the words list
-            words.extend(pattern_words)
+            words.extend(pattern_words)      
+                          
             # add the 'tokenized word list' along with the 'tag' to pattern_word_tags_list
-            pattern_word_tags_list.append((pattern_words, intent['tag']))
+            pattern_word_tags_list.append((pattern_words , intent['tag']))
+            
+            
         # Add all tags to the classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
+
             
-    stem_words = get_stem_words(words, ignore_words)
+    stem_words = get_stem_words(words, ignore_words) 
 
     # Remove duplicate words from stem_words
-    stem_words = sorted(list(set(stem_words)))
+    stem_words = set(stem_words)
+
     # sort the stem_words list and classes list
+    stem_words = sorted(list(stem_words))
     classes = sorted(classes)
-    
+
     # print stem_words
-    print('stem_words list : ', stem_words)
+    print('stem_words list : ' , stem_words)
 
     return stem_words, classes, pattern_word_tags_list
+
 
 # Training Dataset: 
 # Input Text----> as Bag of Words 
 # Tags-----------> as Label
 
 def bag_of_words_encoding(stem_words, pattern_word_tags_list):
+    
     bag = []
     for word_tags in pattern_word_tags_list:
-        # example: word_tags = (['hi', 'there'], 'greetings')
-        pattern_words = word_tags[0]  # ['Hi', 'There']
+        # example: word_tags = (['hi', 'there'], 'greetings']
+
+        pattern_words = word_tags[0] # ['Hi' , 'There]
         bag_of_words = []
 
         # stemming pattern words before creating Bag of words
         stemmed_pattern_word = get_stem_words(pattern_words, ignore_words)
 
-        # Input data encoding
+        # Input data encoding 
         for word in stem_words:
             if word in stemmed_pattern_word:
                 bag_of_words.append(1)
@@ -90,14 +115,18 @@ def bag_of_words_encoding(stem_words, pattern_word_tags_list):
     return np.array(bag)
 
 def class_label_encoding(classes, pattern_word_tags_list):
+    
     labels = []
 
     for word_tags in pattern_word_tags_list:
-        # Start with list of 0s 
-        labels_encoding = list([0] * len(classes))  
 
-        # example: word_tags = (['hi', 'there'], 'greetings')
-        tag = word_tags[1]  # 'greetings'
+        # Start with list of 0s 
+        labels_encoding = list([0]*len(classes))  
+
+        # example: word_tags = (['hi', 'there'], 'greetings']
+
+        tag = word_tags[1]   # 'greetings'
+
         tag_index = classes.index(tag)
 
         # Labels Encoding
@@ -108,21 +137,22 @@ def class_label_encoding(classes, pattern_word_tags_list):
     return np.array(labels)
 
 def preprocess_train_data():
+  
     stem_words, tag_classes, word_tags_list = create_bot_corpus(words, classes, pattern_word_tags_list, ignore_words)
     
-    # Convert Stem words and Classes to Python pickle file format
-    with open('words.pkl', 'wb') as f:
-        pickle.dump(stem_words, f)
-    with open('classes.pkl', 'wb') as f:
-        pickle.dump(tag_classes, f)
+    # Convert Stem words and Classes to Python pickel file format
+    pickle.dump(stem_words, open('words.pkl','wb'))
+    pickle.dump(tag_classes, open('classes.pkl','wb'))
 
     train_x = bag_of_words_encoding(stem_words, word_tags_list)
     train_y = class_label_encoding(tag_classes, word_tags_list)
     
     return train_x, train_y
 
-bow_data, label_data = preprocess_train_data()
+bow_data  , label_data = preprocess_train_data()
+
+# after completing the code, remove comment from print statements
+print("first BOW encoding: " , bow_data[0])
+print("first Label encoding: " , label_data[0])
 
 
-print("first BOW encoding: ", bow_data[0])
-print("first Label encoding: ", label_data[0])
